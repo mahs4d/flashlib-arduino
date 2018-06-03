ab2386eb10541b92ff14978b2148ca6f14a6fe48
Mahdi Sadeghi <persiaware@gmail.com>
2018-06-03 23:52:25 +0430

    flasher, processor and usb processor are added

---
+++ flashlib-flasher/flashlib-flasher.ino
@@ -0,0 +1,53 @@
+#include "flasher.h"
+
+#define RESET_PIN 9
+
+Flasher flasher;
+extEEPROM eeprom = extEEPROM(kbits_256, 1, 64);
+
+void setup()
+{
+	setupEEPROM();
+	setupFlasher();
+	Serial.println("Setup Complete");
+  if (flasher.flash(EEPROM_OFFSET_ADDRESS, 2758 - EEPROM_OFFSET_ADDRESS) != OK)
+  {
+    Serial.println("Flash Failed");
+    halt();
+  }
+  Serial.println("Flash Completed");
+}
+
+void setupEEPROM()
+{
+	if (eeprom.begin(twiClock400kHz) != 0)
+	{
+		Serial.println("Eeprom Setup Failed");
+		halt();
+	}
+}
+
+void setupFlasher()
+{
+	if (flasher.setup(&Serial, &eeprom, RESET_PIN) != OK)
+	{
+		Serial.println("FlashLib Setup Failed");
+		halt();
+	}
+}
+
+void loop()
+{
+
+}
+
+void halt()
+{
+	while(true)
+	{
+		digitalWrite(LED_BUILTIN, HIGH);
+		delay(300);
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(300);
+	}
+}
---
+++ flashlib-usb/flashlib-usb.ino
@@ -0,0 +1,115 @@
+#include "processor.h"
+#include <extEEPROM.h>
+#include <UsbFat.h>
+#include <masstorage.h>
+
+Processor processor;
+extEEPROM eeprom = extEEPROM(kbits_256, 1, 64);
+USB usb;
+BulkOnly bulk(&usb);
+UsbFat key(&bulk);
+
+uint8_t packet[PACKET_SIZE];
+
+void setup()
+{
+  Serial.begin(OPTIBOOT_BAUD_RATE);
+  Serial.println("Setup");
+  
+  setupBasics();
+  setupEeprom();
+  setupUsb();
+  setupProcessor();
+  startProcessing("b.hex");
+  
+  Serial.println("Setup Complete");
+}
+
+void setupBasics()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void setupEeprom()
+{
+  if (eeprom.begin(twiClock400kHz) != 0)
+  {
+    Serial.println("Eeprom Initialization Failed");
+    halt();
+  }
+  
+  Serial.println("Eeprom Initialized");
+}
+
+void setupUsb()
+{
+  if (!initUSB(&usb))
+  {
+    Serial.println("Usb Initialization Failed");
+    halt();
+  }
+
+  if (!key.begin())
+  {
+    Serial.println("Filesystem Initialization Failed");
+    halt();
+  }
+  
+  Serial.println("Usb Initialized");
+}
+
+void setupProcessor()
+{
+  processor.setup(&eeprom);
+}
+
+// Main --------------------------------------------------------
+
+void loop() 
+{
+  
+}
+
+void startProcessing(const char* filename)
+{
+  processor.reset();
+  
+  if (!key.exists(filename))
+  {
+    Serial.println("File doesn't exist");
+    return;
+  }
+  
+  Serial.println("processing...");
+  
+  File file(filename, FILE_READ);
+  size_t readbytes;
+  while (readbytes = file.read(packet, PACKET_SIZE))
+  {
+    if (processor.process(packet, readbytes) != OK)
+    {
+      Serial.println("error in writing to eeprom!");
+      processor.reset();
+      file.close();
+      return;
+    }
+  }
+  file.close();
+
+  Serial.println(processor.maxEepromAddress);
+}
+
+// Helpers -----------------------------------------------------
+
+void halt()
+{
+  while(true)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(300);
+  }
+}
+
